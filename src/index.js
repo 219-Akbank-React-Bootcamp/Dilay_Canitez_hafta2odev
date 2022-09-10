98c537259c60202a2630f2e97dded8dcd116f6e0
Dilay CANITEZ <dilay.canitez@one37id.com>
2022-09-11 01:13:23 +0300

    product transfer

--- src/index.js
+++ src/index.js
@@ -64,11 +64,31 @@ const listProducts = () => {
 };
 const sellProduct = () => {
   const product = state.productList.find((x) => x.id === state.currentProduct);
-  if (product) {
-    console.log(product);
+  const user = state.userList.find((x) => x.id === state.currentUser);
+  if (product && user.balance >= product.price) {
+    const userProduct = user.products.find(
+      (x) => x.id === state.currentProduct
+    );
+    if (userProduct) {
+      userProduct.quantity += 1;
+    } else {
+      user.products = [...user.products, { ...product, quantity: 1 }];
+    }
+    user.balance -= product.price;
+    product.quantity -= 1;
+    const newHistory = {
+      id: Math.round(Math.random() * 1000),
+      userId: user.id,
+      createdAt: new Date().toLocaleString(),
+      details: `user purchased ${product.name}`,
+    };
+    setState("history", newHistory);
   } else {
     alert("Please select a product from the list ");
   }
+  updateUserTable();
+  updateProductTable();
+  updateSelectOptions();
 };
 
 //render functions
@@ -100,20 +120,22 @@ const updateProductTable = () => {
 const updateSelectOptions = () => {
   //update sender-receiver fields
   document.querySelectorAll(".userlist").forEach((element) => {
-    element.innerHTML = state.userList
+    element.innerHTML = `<option value="">Choose Product..</option> ${state.userList
       .map(
         (user) =>
           `<option value="${user.id}">${user.name} ${user.surname}</option>`
       )
-      .join(" ");
+      .join(" ")}`;
   });
 };
 const updateProductSelectOption = () => {
   const senderId = parseInt(document.getElementById("sales_sender").value);
   const senderUser = state.userList.find((x) => x.id === senderId);
-  document.querySelector(".productlist").innerHTML = senderUser.products
+  document.querySelector(
+    ".productlist"
+  ).innerHTML = `<option value="">Choose..</option>${senderUser.products
     .map((product) => `<option value="${product.id}">${product.name}</option>`)
-    .join(" ");
+    .join(" ")}`;
 };
 
 const handleAddUser = (e) => {
@@ -145,15 +167,17 @@ const handleAddUser = (e) => {
 
 const handleAddProduct = (e) => {
   e.preventDefault();
-  const productName = document.getElementById("product_name");
-  const productQuantity = document.getElementById("product_quantity");
-  const productPrice = document.getElementById("product_price");
+  const productName = document.getElementById("product_name").value;
+  const productQuantity = parseInt(
+    document.getElementById("product_quantity").value
+  );
+  const productPrice = parseInt(document.getElementById("product_price").value);
 
   const newProduct = {
     id: Math.round(Math.random() * 1000),
-    name: productName.value,
-    quantity: productQuantity.value,
-    price: productPrice.value,
+    name: productName,
+    quantity: productQuantity,
+    price: productPrice,
   };
   setState("productList", newProduct);
   updateProductTable();
@@ -200,34 +224,46 @@ const handleSell = (e) => {
   const senderId = parseInt(document.getElementById("sales_sender").value);
   const receiverId = parseInt(document.getElementById("sales_receiver").value);
   const productId = parseInt(document.getElementById("sales_product").value);
-  //make the transfer
+  //make the product sell
   const senderUser = state.userList.find((x) => x.id === senderId);
   const receiverUser = state.userList.find((x) => x.id === receiverId);
-  const soldProduct = state.productList.find((x) => x.id === productId);
+  const soldProduct = senderUser.products.find((x) => x.id === productId);
 
-  if (senderUser.balance >= soldProduct.price) {
-    senderUser.balance -= amount;
-    receiverUser.balance += amount;
-    //create history for the transaction
+  if (receiverUser.balance > soldProduct.price) {
+    senderUser.balance += amount;
+    receiverUser.balance -= amount;
+    soldProduct.quantity -= 1;
+    const receivedProduct = receiverUser.products.find(
+      (x) => x.id === productId
+    );
+    if (receivedProduct) {
+      receivedProduct.quantity += 1;
+    } else {
+      receiverUser.products = [
+        ...receiverUser.products,
+        { ...soldProduct, quantity: 1 },
+      ];
+    }
     const newHistory = {
       id: Math.round(Math.random() * 1000),
       userId: senderUser.id,
       createdAt: new Date().toLocaleString(),
-      details: `user send $ ${amount} to the user with id ${receiverUser.id}`,
+      details: `user sold ${soldProduct.name} to the user with id ${receiverUser.id}`,
     };
     const newHistory2 = {
       id: Math.round(Math.random() * 1000),
       userId: receiverUser.id,
       createdAt: new Date().toLocaleString(),
-      details: `user received $ ${amount} from the user with id ${senderUser.id}`,
+      details: `user purchased ${soldProduct.name} from the user with id ${senderUser.id}`,
     };
     setState("history", newHistory);
     setState("history", newHistory2);
   } else {
     alert(
-      "You cannot make the transfer, the user doesn't have enough balance to make this transfer."
+      "You cannot make the transfer, the user doesn't have enough balance to make this purchase."
     );
   }
+
   updateUserTable();
   updateSelectOptions();
 };
